f64e1cf460a5ad7efdc7f0114bff3517e3879e80
unknown <sbarrera.96@hotmail.com>
2021-11-28 18:13:37 -0500

    Code working

--- App Biblioteca/frontend/src/components/Simulador.jsx
+++ App Biblioteca/frontend/src/components/Simulador.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 //import Button from 'react-bootstrap/Button';
 //import Cards from 'react-bootstrap/Cards';
 //import { Link } from 'react-router-dom';
@@ -7,24 +7,24 @@ import React, {useState} from 'react'
 
 
 const Simulador = () => {
-    
-    const[valordelprestamo, setValordelprestamo]=useState("")
-    const[cuotas, setCuotas]=useState("")
-    const[tdi, setTdi]=useState("")
-    const[valor, setValor]=useState("")
-    
 
-    const calcularcuota = async(e) => {
+    const [valordelprestamo, setValordelprestamo] = useState("")
+    const [cuotas, setCuotas] = useState("")
+    const [tdi, setTdi] = useState("")
+    const [valor, setValor] = useState("")
+
+
+    const calcularcuota = async (e) => {
 
         // se hace la formula considerando el siguiente link https://www.youtube.com/watch?v=N1-i1pXYXOE&t=161s
-         const cuotaestimada = (valordelprestamo*(tdi/100))/(1- ((1+(tdi/100)))**(cuotas*-1));
-         setValor(cuotaestimada)
-         alert("La cuota es: "+cuotaestimada); 
-        }
+        const cuotaestimada = (valordelprestamo * (tdi / 100)) / (1 - ((1 + (tdi / 100))) ** (cuotas * -1));
+        setValor(cuotaestimada)
+        alert("La cuota es: " + cuotaestimada);
+    }
 
 
-    const validar=(event)=>{
-        
+    const validar = (event) => {
+
 
         //capturar el evento submit del formulario y evitar que la pagina se recargue
         event.preventDefault()
@@ -32,110 +32,99 @@ const Simulador = () => {
         //trim evalua si hay cadena de caracteres
 
 
-        if (!valordelprestamo.trim()){
-            console.log ("El valor del prestamo esta vacio, por favor ingresa la informacion solicitada")
+        if (!valordelprestamo.trim()) {
+            console.log("El valor del prestamo esta vacio, por favor ingresa la informacion solicitada")
             return
         }
 
-        if (!cuotas.trim()){
-            console.log ("La cantidad de cuotas esta vacia,por favor ingresa la informacion solicitada ")
+        if (!cuotas.trim()) {
+            console.log("La cantidad de cuotas esta vacia,por favor ingresa la informacion solicitada ")
             return
         }
 
-        if (!tdi.trim()){
-            console.log ("La tasa efectiva mensual esta vacia,por favor ingresa la informacion solicitada ")
+        if (!tdi.trim()) {
+            console.log("La tasa efectiva mensual esta vacia,por favor ingresa la informacion solicitada ")
             return
         }
-        
 
-    }        
+
+    }
 
 
 
     return (
-<<<<<<< HEAD
         <div className="container">
-        <div className="row-4">
-        <div className="col-4">
-    
-        </div>
-        
-        <div className="row-4"></div>
-        <div className="row"></div>
-=======
-        <div class="container mt-4">
-        <div class="row-4">
-        <div class="col-4">
-    
-        </div>
+            <div className="row-4">
+                <div className="col-4">
 
-        <div class="row-4"></div>
-        <div class="row"></div>
->>>>>>> 235627c7b9cf02e265673a256ddb1ec726e3a1a6
+                </div>
 
-        <div class="col"></div>
-        <div className="container">
-            <header>
+                <div className="row-4"></div>
+                <div className="row"></div>
+
+                <div className="col"></div>
                 <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                            <h1><i className="fas -alt-text-center-font-weight-bold">            Simulador de préstamos</i></h1>
+                    <header className='py-2'>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <h1>Simulador de préstamos</h1>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </header>
+                    <div className="col"></div>
+                    <div className="row"></div>
+                    <div className="row"></div>
+
+
                 </div>
-            </header>
-            <div class="col"></div>
-            <div class="row"></div>
-            <div class="row"></div>
-
-             
-        </div>
-        <div className="container">
+                <div className="container">
 
 
-            <form onSubmit={validar} className="form-groups">
-                <input
-                placeholder="Introduce el valor del prestamo"
-                className="form-control mb-3"
-                type="number"
-                 //Recibe un evento que es le evento del input 
-                //y dentro de esta funcion 
-                //Se recoge en valor del input y lo setea dentro de la constante el estado
-                //target una referencia al objeto en el cual se lanzo el evento. 
-                onChange= {(e)=>{setValordelprestamo(e.target.value)}}
-                />
+                    <form onSubmit={validar} className="form-groups">
+                        <input
+                            placeholder="Introduce el valor del prestamo"
+                            className="form-control mb-3"
+                            type="number"
+                            //Recibe un evento que es le evento del input 
+                            //y dentro de esta funcion 
+                            //Se recoge en valor del input y lo setea dentro de la constante el estado
+                            //target una referencia al objeto en el cual se lanzo el evento. 
+                            onChange={(e) => { setValordelprestamo(e.target.value) }}
+                        />
 
-                <input
-                placeholder="Introduce la cantidad de cuotas"
-                className="form-control mb-3"
-                type="number"
-                onChange= {(e)=>{setCuotas(e.target.value)}}
-                />
+                        <input
+                            placeholder="Introduce la cantidad de cuotas"
+                            className="form-control mb-3"
+                            type="number"
+                            onChange={(e) => { setCuotas(e.target.value) }}
+                        />
 
-                <input
-                placeholder="Introduce la tasa de interes efectiva mensual o anual segun desee calcular la cuota"
-                className="form-control mb-3"
-                type="numner"
-                onChange= {(e)=>{setTdi(e.target.value)}}
-                />
+                        <input
+                            placeholder="Introduce la tasa de interes efectiva mensual o anual segun desee calcular la cuota"
+                            className="form-control mb-3"
+                            type="numner"
+                            onChange={(e) => { setTdi(e.target.value) }}
+                        />
 
-                <button className="btn btn-primary btn-info btn-block " type="submit" onClick={()=>calcularcuota()}>Calcular</button>
-            
-               </form>
-            </div>
-            <div className="col"></div>
+                        <button className="btn btn-primary btn-info btn-block " type="submit" onClick={() => calcularcuota()}>Calcular</button>
+
+                    </form>
+                </div>
+                <div className="col"></div>
             </div>
 
-                <div>
+            <div>
                 <nav aria-label="breadcrumbmt-4">
-                <ol class="breadcrumb mt-4">
-                <li class="breadcrumb-item activemt-4" aria-current="page"> La cuota estimada es de ${valor}</li>
-                </ol>
+                    <ol className="breadcrumb mt-4">
+                        <li className="breadcrumb-item activemt-4" aria-current="page"> La cuota estimada es de ${valor}</li>
+                    </ol>
                 </nav>
-                 
-                </div>
 
-             {/* Card * guiarse de inicio sesion*/}
+            </div>
+
+            {/* Card * guiarse de inicio sesion*/}
 
         </div>
     )
